fbac5056a8817d8548c841ee0a0d4ad74b511996
LucelhaSantos <lucelha_santos@msn.com>
2024-09-19 14:43:05 -0300

    #soma

--- exercicio1.c
+++ exercicio1.c
@@ -1,13 +1,16 @@
-int INDICE = 13, SOMA = 0, K = 0;
+int main() {
+  int INDICE = 13, SOMA = 0, K = 0;
 
 while (K < INDICE) {
     K = K + 1;
     SOMA = SOMA + K;
 }
 
-printf("%d", SOMA); // O valor de SOMA será impresso.
+printf("O valor da soma é:%d\n", SOMA); 
 
+return 0;
 
+}
 
 
 //Explicação:
