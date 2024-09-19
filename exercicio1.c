b5eba7052ff4a0e1976800bb3bfdf012767525f8
LucelhaSantos <lucelha_santos@msn.com>
2024-09-19 13:45:40 -0300

    #soma1

--- exercicio1.c
+++ exercicio1.c
@@ -6,3 +6,15 @@ while (K < INDICE) {
 }
 
 printf("%d", SOMA); // O valor de SOMA será impresso.
+
+
+
+
+//Explicação:
+
+K começa com 0 e é incrementado até 13.
+A cada iteração, o valor de K é somado a SOMA.
+Após o término, o valor de SOMA será a soma de todos os números de 1 a 13, ou seja:
+
+SOMA=1+2+3+⋯+13=91
+Resultado: O valor de SOMA será 91. //
