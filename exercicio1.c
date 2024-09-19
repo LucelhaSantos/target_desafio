f9ee9cbfc4262e177aef98c18ce1e46bfe1f49fb
LucelhaSantos <lucelha_santos@msn.com>
2024-09-19 15:34:03 -0300

    #soma

--- exercicio1.c
+++ exercicio1.c
@@ -15,11 +15,3 @@ return 0;
 }
 
 
-//Explicação:
-
-K começa com 0 e é incrementado até 13.
-A cada iteração, o valor de K é somado a SOMA.
-Após o término, o valor de SOMA será a soma de todos os números de 1 a 13, ou seja:
-
-SOMA=1+2+3+⋯+13=91
-Resultado: O valor de SOMA será 91. //
