82b0825852a9332f17a099f78dc82e2ea6c956cb
LucelhaSantos <lucelha_santos@msn.com>
2024-09-24 20:22:01 -0300

    #faturamentodiario

--- exercicio3.py
+++ exercicio3.py
@@ -1,19 +1,8 @@
 import json
 
-faturamento_diario = '''{
-    "dias": [
-        {"dia": 1, "valor": 1000.0},
-        {"dia": 2, "valor": 2000.0},
-        {"dia": 3, "valor": 0.0},    # sem faturamento (feriado ou fim de semana)
-        {"dia": 4, "valor": 1500.0},
-        {"dia": 5, "valor": 3000.0},
-        {"dia": 6, "valor": 0.0},    # sem faturamento (feriado ou fim de semana)
-        {"dia": 7, "valor": 1700.0}
-    ]
-}'''
-
-# Carregar os dados de faturamento do JSON
-dados = json.loads(faturamento_diario)
+# Carregar os dados de faturamento de um arquivo JSON
+with open('faturamento_diario.json', 'r') as f:
+    dados = json.load(f)
 
 # Filtrar apenas os dias com faturamento
 faturamentos = [dia["valor"] for dia in dados["dias"] if dia["valor"] > 0]
